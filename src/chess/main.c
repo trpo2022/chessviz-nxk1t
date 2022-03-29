c5ee27a75e2dfde83c84790123f3ef4683ef6bb7
Патрушев Никита Владимирович <samfishernik@gmail.com>
2022-03-30 02:26:44 +0700

    Create new main

--- src/chess/main.c
+++ src/chess/main.c
@@ -2,12 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <libchess/Fill_Table.h>
+#include <libchess/Input.h>
+#include <libchess/Move.h>
+#include <libchess/PrintTable.h>
+
 #define n 8
+#define step 11
 
 int main()
 {
-    PrintTable();
-    Translate_to_numbers();
+    int** chess;
+    char* input_step;
+
+    chess = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+        chess[i] = (int*)malloc(n * sizeof(int));
+    input_step = (char*)malloc(step * sizeof(char));
+
+    Fill_Table(chess);
+    PrintTable(chess);
+    Input(input_step);
+
+    printf("\n");
+    for (int i = 0; i < step; i++)
+        printf("%c", input_step[i]);
+    Move(chess, input_step);
+    printf("\n");
+    PrintTable(chess);
 
     return 0;
 }
