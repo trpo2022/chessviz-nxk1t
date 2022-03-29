51191782504c3d65fae13f0ab282653faeb73f21
Патрушев Никита Владимирович <samfishernik@gmail.com>
2022-03-30 02:37:15 +0700

    Create folder

---
+++ obj/src/chess/main.d
@@ -0,0 +1,10 @@
+obj/src/chess/main.o: src/chess/main.c src/libchess/Fill_Table.h \
+ src/libchess/Input.h src/libchess/Move.h src/libchess/PrintTable.h
+
+src/libchess/Fill_Table.h:
+
+src/libchess/Input.h:
+
+src/libchess/Move.h:
+
+src/libchess/PrintTable.h:
