083764c8d82a5734bb04e0dfc01c7dcd3b418f6c
Патрушев Никита Владимирович <samfishernik@gmail.com>
2022-05-08 00:19:04 +0700

    Fixed main.c

--- src/chess/main.c
+++ src/chess/main.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <libchess/Fill_Table.h>
-#include <libchess/Input.h>
-#include <libchess/Move.h>
-#include <libchess/PrintTable.h>
+#include <libchess/check_move.h>
+#include <libchess/enum.h>
+#include <libchess/fill_table.h>
+#include <libchess/find_position_black.h>
+#include <libchess/find_position_white.h>
+#include <libchess/input.h>
+#include <libchess/move.h>
+#include <libchess/print_table.h>
 
 #define n 8
 #define step 12
@@ -14,22 +18,25 @@ int main()
 {
     int** chess;
     char* input_step;
+    int flag = 1;
 
     chess = (int**)malloc(n * sizeof(int*));
     for (int i = 0; i < n; i++)
         chess[i] = (int*)malloc(n * sizeof(int));
     input_step = (char*)malloc(step * sizeof(char));
-
-    Fill_Table(chess);
-    PrintTable(chess);
-    Input(input_step);
-
+    fill_table(chess);
+    print_table(chess);
+    while (flag == 1) {
+        printf("\nEnter white and black steps: ");
+        input(input_step);
+        flag = check_move(input_step);
+    }
     printf("\n");
-    for (int i = 0; i < step; i++)
-        printf("%c", input_step[i]);
-    Move(chess, input_step);
+
+    find_position_white(chess, input_step);
+    find_position_black(chess, input_step);
     printf("\n");
-    PrintTable(chess);
+    print_table(chess);
 
     return 0;
 }
